131c7482ba8de2aef75f65398177b1146950917c
restful-otaku <j-m-harrison@protonmail.com>
2026-07-03 21:52:23 +0100

    Minor script build fixes  for publishing, client gen and migrations

--- BitCraftServer/packages/game/generate-client-files.sh
+++ BitCraftServer/packages/game/generate-client-files.sh
@@ -14,10 +14,10 @@ done
 
 if [ "$language" = "ts" ]; then
   echo "Generating typecript files"
-  spacetime generate --out-dir ../../../../bitcraft-login-server/login-server/src/autogen/ --lang=ts --namespace=Bitcraft.Spacetime
+  spacetime generate --module-path . --out-dir ../../../../bitcraft-login-server/login-server/src/autogen/ --lang=ts --namespace=Bitcraft.Spacetime
 else
   echo "Deleting C# files"
   rm -rf ../../../Assets/_Project/autogen/*.cs
   echo "Generating C# files"
-  spacetime generate --out-dir ../../../Assets/_Project/autogen --lang=cs --namespace=BitCraft.Spacetime
+  spacetime generate --module-path . --out-dir ../../../Assets/_Project/autogen --lang=cs --namespace=BitCraft.Spacetime
 fi
--- BitCraftServer/packages/global_module/generate-client-files.sh
+++ BitCraftServer/packages/global_module/generate-client-files.sh
@@ -3,4 +3,4 @@
 echo "Deleting C# files"
 rm -rf ../../../Assets/_Project/autogen-global/*.cs
 echo "Generating C# files"
-spacetime generate --out-dir ../../../Assets/_Project/autogen-global --lang=cs --namespace=BitCraft.Global
+spacetime generate --out-dir ../../../Assets/_Project/autogen-global --lang=cs --namespace=BitCraft.Global --module-path .
